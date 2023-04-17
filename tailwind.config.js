e465697c8f4883d9761f2118f52ca399f238b908
Antony <antony.jacques@laposte.net>
2023-04-17 11:30:35 +0200

    tailwind custom colors and ont

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,16 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts}"],
   theme: {
     extend: {
+      // extend tailwind functionality
       fontFamily: {
         sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
-      }
+      },
+      colors: {
+        "brand-gray-1": "#dadce0", // add custom class
+        "brand-blue-1": "#1967d2", // add custom class
+        "brand-green-1": "#137333", // add custom class
+      },
     },
   },
   plugins: [],
 };
-
