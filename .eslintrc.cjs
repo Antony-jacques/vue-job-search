27bbceec380ca5902b3dd09923cd5da2a2c5a08a
Antony <antony.jacques@laposte.net>
2023-04-18 11:33:27 +0200

    fix eslint errors (58)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "@vue/eslint-config-prettier",
+    "plugin:vitest-globals/recommended",
   ],
   rules: {
     // "prettier/prettier": 0,
@@ -19,5 +20,8 @@ module.exports = {
   },
   parserOptions: {
     ecmaVersion: "latest",
+  },
+  env: {
+    "vitest-globals-env": true,
   },
 };
